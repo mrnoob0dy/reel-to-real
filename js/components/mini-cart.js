cda1144aec85dd60518de9e5b2941b45e9f46aa1
mrnoob0dy <jayna5155@yandex.ru>
2025-09-09 12:51:19 +0300

    mini-cart

---
+++ js/components/mini-cart.js
@@ -0,0 +1,17 @@
+const miniCart = document.querySelector(".mini-cart");
+
+if (miniCart) {
+  const miniCartBtn = document.querySelector(".header__cart");
+  const miniCartClose = document.querySelector(".mini-cart__close");
+  const overlay = document.querySelector(".overlay");
+
+  miniCartBtn.addEventListener("click", () => {
+    miniCart.classList.toggle("mini-cart--active");
+    overlay.classList.toggle("overlay--visible");
+  });
+
+  miniCartClose.addEventListener("click", () => {
+    miniCart.classList.remove("mini-cart--active");
+    overlay.classList.remove("overlay--visible");
+  });
+}
